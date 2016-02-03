2d27d1b89106e320b07bebacb13b3e4dc006f3c8
Michael Gorman <michael@michaeljgorman.com>
2016-02-03 15:05:06 -0800

    Add stat calculation and message preparation.

--- twctattler/tattler.py
+++ twctattler/tattler.py
@@ -1,5 +1,5 @@
 import os, json, ping, sendgrid
-from datetime import datetime
+from datetime import datetime, timedelta
 import sqlite3 as sqlite
 
 class twctattler:
@@ -24,12 +24,26 @@ class twctattler:
         with open(os.path.join(os.path.expanduser('~'),'.sendgrid')) as data_file:
             return json.load(data_file)
 
+    def get_stats(self):
+        query = 'SELECT * FROM Tattler_Results where date >= "{0}";'.format(str(datetime.utcnow()-timedelta(days=1)))
+        self.cur.execute(query)
+        return self.cur.fetchall()
+
+    def calc_stats(self):
+        stats = self.get_stats()
+        failures = [date for tar,date,res,resp in self.get_stats() if not bool(res)]
+        success_rate = len([res for tar,date,res,resp in self.get_stats() if bool(res)])
+        output = ("Failures: {0}".format(failures),
+                "Success: {0}% of {1} checks".format((success_rate/len(stats))*100,len(stats)))
+        return output
+
+
     def send_stats(self):
         sg = sendgrid.SendGridClient(self.sendgrid_info['api-key'])
         message = sendgrid.Mail()
         message.set_from(self.sendgrid_info['email-from'])
         message.add_to(self.sendgrid_info['email-to'])
         message.set_subject('Tattler Stats')
-        message.set_text('This would be Your stats')
+        message.set_html("<html><body><p>Results from the last 24 hours</p><p>{0}</p><p>{1}</p></body></html>".format(*self.calc_stats()))
         status, msg = sg.send(message)
         print "Stats sent! {0}, {1}".format(status, msg)
